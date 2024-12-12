246a9e5333e7464af720e461445c0b0e4f3affee
clavalpe <clavalpe@upv.es>
2024-12-12 20:53:17 +0100

    green - test_it_sums_two_numbers

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -2,4 +2,13 @@ class StringCalculator:
     def add(self, numbers: str):
         if not numbers:
             return 0
-        return int(numbers)   
+        
+        if len(numbers) == 1:
+            return int(numbers)   
+        
+        list_of_numbers = numbers.split(',')
+        sum = 0
+        for number in list_of_numbers:
+            sum +=  int(number)
+
+        return sum
