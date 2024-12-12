6366dd9b69b2b59d1403661da13e0c3a16ec1fca
clavalpe <clavalpe@upv.es>
2024-12-12 20:42:57 +0100

    red - test_when_it_receives_one_string_number_it_returns_the_number

--- src/test/test_string_calculator.py
+++ src/test/test_string_calculator.py
@@ -4,6 +4,14 @@ from string_calculator import StringCalculator
 class TestStringCalculator:
     def test_it_returns_0_when_passed_the_empty_string(self):
         numbers = ''
+
         actual_result = StringCalculator().add(numbers)
 
         assert actual_result == 0
+
+    def test_when_it_receives_one_string_number_it_returns_the_number(self):
+        numbers = '4'
+        
+        actual_result = StringCalculator().add(numbers)
+
+        assert actual_result == 4
