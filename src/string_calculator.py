261f423d9c0e34e22e3a1bf87a2bc24aed309ce6
clavalpe <clavalpe@upv.es>
2024-12-12 20:56:01 +0100

    blue - extract to method, adding typing

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,11 +1,14 @@
 class StringCalculator:
-    def add(self, numbers: str):
+    def add(self, numbers: str) -> int:
         if not numbers:
             return 0
         
         if len(numbers) == 1:
             return int(numbers)   
         
+        return self._sum_numbers(numbers)
+
+    def _sum_numbers(self, numbers: str) -> int:
         list_of_numbers = numbers.split(',')
         sum = 0
         for number in list_of_numbers:
