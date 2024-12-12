5043a2b1a9f15d53a8df0a897e5053b607d7f152
clavalpe <clavalpe@upv.es>
2024-12-12 21:26:19 +0100

    green - test_it_recognises_newlines_and_commas_as_separators

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -6,6 +6,7 @@ class StringCalculator:
         return self._sum_numbers(numbers)
 
     def _sum_numbers(self, numbers: str) -> int:
+        numbers = numbers.replace('\n', ',')
         list_of_numbers = numbers.split(',')
         sum = 0
         for number in list_of_numbers:
